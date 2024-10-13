35d8002543b8051400a5fc9d6591fb5101bfe806
kenneth-l-yan <kennethyantheking@gmail.com>
2024-10-13 17:01:31 -0400

    Added url shortener folder
    Added folder containing html, css, and js files.

---
+++ URL Shorterner/script.js
@@ -0,0 +1,7 @@
+let urlToShorten;
+function printOutput(){
+    urlToShorten = document.getElementById("userInputBox").value;
+    document.getElementById("outputText").textContent = `Your shortened URL: "${urlToShorten}"`;
+};
+
+window.onload = hello.html;
