a00182fc940e9ebb0252be419e3821057371f599
kenneth-l-yan <kennethyantheking@gmail.com>
2024-10-17 01:57:36 -0400

    Delete URL Shorterner directory

--- URL Shorterner/script.js
+++
@@ -1,7 +0,0 @@
-let urlToShorten;
-function printOutput(){
-    urlToShorten = document.getElementById("userInputBox").value;
-    document.getElementById("outputText").textContent = `Your shortened URL: "${urlToShorten}"`;
-};
-
-window.onload = hello.html;
