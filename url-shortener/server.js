97dbdc3bd0e55f0faf04c100afe9ab5a6ea52c29
kenneth-l-yan <kennethyantheking@gmail.com>
2024-11-02 00:02:47 -0400

    small change

--- url-shortener/server.js
+++ url-shortener/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//Set the view engine
 app.use(express.urlencoded({ extended: true}));
 app.set('view engine', 'ejs');
 
